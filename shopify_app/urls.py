0bd7646937b77a6fe4b4fb16c040c0609655bf7f
tanish vashisth <80052733+bogusdeck@users.noreply.github.com>
2025-03-29 19:18:08 +0000

    app name fix

--- shopify_app/urls.py
+++ shopify_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from .views import customer_data_request, customer_data_erasure, shop_data_erasure
 from shopify_app.management.commands.DOUBLEpopulate_plans import last_algo_create_time
-from shopify_django_app.mongodb import faq_list, test_mongodb_connection,status_list, history_status
+# from Sortaroo.mongodb import faq_list, test_mongodb_connection,status_list, history_status
 
 
 urlpatterns = [
@@ -16,10 +16,10 @@ urlpatterns = [
     path('webhooks/customer-data-erasure/', customer_data_erasure, name='customer-data-erasure'),
     path('webhooks/shop-data-erasure/', shop_data_erasure, name='shop-data-erasure'),
 
-    # FAQs
-    path('faqs/', faq_list, name='faq_list'),
-    path('test-mongodb/', test_mongodb_connection,name='test_mongo'),
-    path('api/last_algo_create_time/', last_algo_create_time, name='last_algo_create_time'),
-    # path('status/', status_list, name='fake_status_list'),
-    path('history-status/',history_status, name="history_status")
+    # # FAQs
+    # path('faqs/', faq_list, name='faq_list'),
+    # path('test-mongodb/', test_mongodb_connection,name='test_mongo'),
+    # path('api/last_algo_create_time/', last_algo_create_time, name='last_algo_create_time'),
+    # # path('status/', status_list, name='fake_status_list'),
+    # path('history-status/',history_status, name="history_status")
 ]
